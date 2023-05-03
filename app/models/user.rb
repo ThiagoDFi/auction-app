7902f97439b7697a4822f1432a91599a1313ffd9
Thiago Dantas Firmino <thiagod.firmino@gmail.com>
2023-05-03 11:13:10 -0300

    Adição do atributo "role" para o User e validação de email para adicionar "admin" ou "customer" ao user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  before_validation :set_admin
+
+  def set_admin
+    if self.email.ends_with?('@leilaodogalpao.com.br')
+      self.role = "admin"
+    end
+  end
 end
