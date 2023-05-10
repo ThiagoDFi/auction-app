15a7c6813cf200cef17416a2d8a41ff212041557
Thiago Dantas Firmino <thiagod.firmino@gmail.com>
2023-05-10 10:43:24 -0300

    Ajustando mensagens

--- spec/system/auction_lot/admin_create_auction_lot_spec.rb
+++ spec/system/auction_lot/admin_create_auction_lot_spec.rb
@@ -48,4 +48,28 @@ describe 'Administrador cadastra lote de leião' do
     expect(page).to have_content "Valor diferença: R$ 200,00"
     expect(page).to have_content "Responsável: pedro@leilaodogalpao.com.br"
   end
+  it 'com falha' do
+    #Arrange
+    admin = User.create!(name: 'Pedro', email: 'pedro@leilaodogalpao.com.br', password: 'password',
+                         registry_code: '31350282081', role: 'admin')
+    product = Product.new(name: 'Tv 40', description: 'Tv de ultima geração LED 4K',
+                         weight: 80, width: 3, height: 60, depth: 5, category: 'Tecnologia')
+    product.photo.attach(io: File.open(Rails.root.join('spec', 'support', 'iphone.jpeg')), filename: 'iphone.jpeg', content_type: 'image/jpeg')
+    product.save!
+
+    #Act
+    login_as(admin)
+    visit root_path
+    click_on 'Lote de leilão'
+    click_on '+ Novo Lote de Leilão'
+    fill_in 'Código do Leilão', with: 'SAMD123456'
+    fill_in 'Data final do lote', with: '20/08/2023'
+    fill_in 'Valor mínimo', with: 1000
+    fill_in 'Valor diferença', with: 200
+    click_on 'Enviar' 
+
+    #Assert
+    expect(page).to have_content "Falha ao criar o lote de leilão"
+    expect(page).to have_content "Código do Leilão Inválido"
+  end
 end
